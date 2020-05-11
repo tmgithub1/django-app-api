05fee91d8b7c21591abe6da30173a12417fc2937
tushar <tushar1.mathur@gmail.com>
2020-05-11 15:42:39 +0530

    added newline char

--- app/app/settings.py
+++ app/app/settings.py
@@ -74,10 +74,18 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'NER_correspondance_dev',
+        'HOST': 'mongodb://localhost:27017/NER_correspondance_dev'
     }
 }
 
